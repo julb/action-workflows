00d940ed199407800fb67a2b186c99ff51eb9b52
Julien Baillagou <julien.baillagou@gmail.com>
2021-02-23 10:53:04 +0100

    Add python3 action files.

---
+++ copy-to-all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+copy() {
+    project_name=$1
+    project_type=$2
+
+    echo "=> $project_name ($project_type)"
+    cp -R common/. ../$project_name/
+    cp -R $project_type/. ../$project_name/
+}
+
+# action-manage-branch
+copy 'action-manage-branch' 'python3-action'
+
+# action-manage-label
+copy 'action-manage-label' 'python3-action'
+
+# action-manage-milestone
+copy 'action-manage-milestone' 'python3-action'
+
+# action-manage-tag
+copy 'action-manage-tag' 'python3-action'
+
+# action-prepare-release
+copy 'action-prepare-release' 'python3-action'
